f8d3725420c8b749ffdcf1af433d93e8a39e3ffc
callmedems <[demmi.zepeda@gmail.com]>
2024-09-24 17:05:41 -0600

    commented the code

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void swap(int& a, int& b) {
+// swap variables
+void swap(int& a, int& b) { 
     int temp = a;
     a = b;
     b = temp;
 }
 
+// partition function 
 int partition(int arr[], int low, int high) {
+    // last element as pivot
     int pivot = arr[high];   
     int i = low - 1;        // index of the smaller element
 
+    // partition the array
     for (int j = low; j < high; j++) {
         if (arr[j] < pivot) {
             i++;
@@ -21,15 +25,18 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
+// QuickSort function
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         int pi = partition(arr, low, high);
 
+        // sort the left and right parts
         quickSort(arr, low, pi - 1);  // sorting the left part
         quickSort(arr, pi + 1, high); // sorting the right part
     }
 }
 
+// print the array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
